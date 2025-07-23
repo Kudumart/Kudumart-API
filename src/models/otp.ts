fbaf961783a1791c28c7b28e616eea4ca0585709
Anuolu-2020 <Anuolu1645@gmail.com>
2025-07-23 17:49:23 +0100

    feat(otp): add otpToken and otpTokenExpiresAt to OTP model

--- src/models/otp.ts
+++ src/models/otp.ts
@@ -3,38 +3,42 @@ import { Model, DataTypes, Sequelize } from "sequelize";
 import User from "./user";
 
 class OTP extends Model {
-  public userId!: string;
-  public otpCode!: string;
-  public expiresAt?: Date;
-  public updatedAt?: Date;
-  public deletedAt?: Date | null;
-  public user?: User;
+	public userId!: string;
+	public otpCode!: string;
+	public expiresAt?: Date;
+	public otpToken!: string;
+	public otpTokenExpiresAt?: Date;
+	public updatedAt?: Date;
+	public deletedAt?: Date | null;
+	public user?: User;
 
-  // Association with User model
-  static associate(models: any) {
-    this.belongsTo(models.User, {
-      as: 'user',
-      foreignKey: 'userId', // Ensure the OTP model has a 'userId' column
-    });
-  }
+	// Association with User model
+	static associate(models: any) {
+		this.belongsTo(models.User, {
+			as: "user",
+			foreignKey: "userId", // Ensure the OTP model has a 'userId' column
+		});
+	}
 }
 
 const initModel = (sequelize: Sequelize) => {
-  OTP.init(
-    {
-      userId: DataTypes.UUID,
-      otpCode: DataTypes.STRING,
-      expiresAt: DataTypes.DATE,
-    },
-    {
-      sequelize,
-      modelName: "OTP",
-      timestamps: true,
-      paranoid: false,
-      tableName: "otps"
-    }
-  );
+	OTP.init(
+		{
+			userId: DataTypes.UUID,
+			otpCode: DataTypes.STRING,
+			otpToken: DataTypes.STRING,
+			otpTokenExpiresAt: DataTypes.DATE,
+			expiresAt: DataTypes.DATE,
+		},
+		{
+			sequelize,
+			modelName: "OTP",
+			timestamps: true,
+			paranoid: false,
+			tableName: "otps",
+		},
+	);
 };
 
-export default OTP; 
+export default OTP;
 export { initModel };
