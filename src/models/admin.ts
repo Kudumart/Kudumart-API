a4cf52480d794726c5c877e5a28cfc8e8cf7a493
Anuolu-2020 <Anuolu1645@gmail.com>
2025-07-29 00:16:54 +0100

    feat(admin): add fcmToken and password hashing hook
    This commit adds the fcmToken field to the Admin model and ensures that
    passwords are properly hashed before saving to the database.

--- src/models/admin.ts
+++ src/models/admin.ts
@@ -3,87 +3,87 @@ import bcrypt from "bcrypt";
 import { Model, DataTypes, Sequelize } from "sequelize";
 
 class Admin extends Model {
-  public id!: string; // Use '!' to indicate these fields are definitely assigned
-  public name!: string;
-  public email!: string;
-  public password!: string;
-  public photo?: string;
-  public roleId?: string;
-  public status?: "active" | "inactive";
-  public createdAt?: Date;
-  public updatedAt?: Date;
+	public id!: string; // Use '!' to indicate these fields are definitely assigned
+	public name!: string;
+	public email!: string;
+	public password!: string;
+	public photo?: string;
+	public roleId?: string;
+	public status?: "active" | "inactive";
+	public fcmToken?: string;
+	public createdAt?: Date;
+	public updatedAt?: Date;
 
-  // Method to hash the password before saving
-  static async hashPassword(password: string): Promise<string> {
-    return await bcrypt.hash(password, 10);
-  }
+	// Method to hash the password before saving
+	static async hashPassword(password: string): Promise<string> {
+		return await bcrypt.hash(password, 10);
+	}
 
-  // Method to check the password
-  checkPassword(password: string): Promise<boolean> {
-    return bcrypt.compare(password, this.password);
-  }
+	// Method to check the password
+	checkPassword(password: string): Promise<boolean> {
+		return bcrypt.compare(password, this.password);
+	}
 
-  hasRole(requiredRole: string) {
-    return this.roleId === requiredRole;
-  }
+	hasRole(requiredRole: string) {
+		return this.roleId === requiredRole;
+	}
 
-  // Association with Role model
-  static associate(models: any) {
-    this.belongsTo(models.Role, {
-      as: 'role',
-      foreignKey: 'roleId',
-    });
-    this.hasMany(models.Store, { 
-      as: 'stores', 
-      foreignKey: 'vendorId',
-      onDelete: 'RESTRICT', 
-    });
-  }
-
+	// Association with Role model
+	static associate(models: any) {
+		this.belongsTo(models.Role, {
+			as: "role",
+			foreignKey: "roleId",
+		});
+		this.hasMany(models.Store, {
+			as: "stores",
+			foreignKey: "vendorId",
+			onDelete: "RESTRICT",
+		});
+	}
 }
 
 const initModel = (sequelize: Sequelize) => {
-  Admin.init(
-    {
-      id: {
-        type: DataTypes.UUID,
-        primaryKey: true,
-        defaultValue: DataTypes.UUIDV4, // Automatically generate UUIDs
-      },
-      name: DataTypes.STRING,
-      email: {
-        type: DataTypes.STRING,
-        unique: true, // Ensure unique emails
-      },
-      password: DataTypes.STRING,
-      photo: DataTypes.TEXT,
-      roleId: DataTypes.UUID,
-      status: DataTypes.ENUM("active", "inactive"),
-    },
-    {
-      sequelize,
-      modelName: "Admin",
-      timestamps: true,
-      paranoid: false,
-      tableName: "admins",
-      defaultScope: {
-        attributes: { exclude: ["password"] },
-      },
-      scopes: {
-        auth: {
-          attributes: { include: ["password"] }, // Add necessary fields for authentication
-        },
-      },
-    }
-  );
+	Admin.init(
+		{
+			id: {
+				type: DataTypes.UUID,
+				primaryKey: true,
+				defaultValue: DataTypes.UUIDV4, // Automatically generate UUIDs
+			},
+			name: DataTypes.STRING,
+			email: {
+				type: DataTypes.STRING,
+				unique: true, // Ensure unique emails
+			},
+			fcmToken: DataTypes.STRING,
+			password: DataTypes.STRING,
+			photo: DataTypes.TEXT,
+			roleId: DataTypes.UUID,
+			status: DataTypes.ENUM("active", "inactive"),
+		},
+		{
+			sequelize,
+			modelName: "Admin",
+			timestamps: true,
+			paranoid: false,
+			tableName: "admins",
+			defaultScope: {
+				attributes: { exclude: ["password"] },
+			},
+			scopes: {
+				auth: {
+					attributes: { include: ["password"] }, // Add necessary fields for authentication
+				},
+			},
+		},
+	);
 
-  // Add the password hashing hook before saving
-  Admin.addHook("beforeSave", async (admin: Admin) => {
-    if (admin.changed("password") || admin.isNewRecord) {
-      admin.password = await Admin.hashPassword(admin.password);
-    }
-  });
-  
+	// Add the password hashing hook before saving
+	Admin.addHook("beforeSave", async (admin: Admin) => {
+		if (admin.changed("password") || admin.isNewRecord) {
+			admin.password = await Admin.hashPassword(admin.password);
+		}
+	});
 };
 
 // Export the User model and the init function
