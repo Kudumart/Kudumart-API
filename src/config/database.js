e49b1434d4089faa760139b472e29a8705dd97e4
Anuolu-2020 <Anuolu1645@gmail.com>
2025-07-29 23:46:47 +0100

    feat(config): add database port configuration

--- src/config/database.js
+++ src/config/database.js
@@ -1,15 +1,16 @@
-require('dotenv/config');
+require("dotenv/config");
 
 module.exports = {
-  dialect: process.env.DB_DIALECT || 'mysql',
-  host: process.env.DB_HOST,
-  username: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME,
-  define: {
-    timestamps: true,
-  },
-  migrationStorage: 'sequelize',
-  migrationStorageTableName: 'SequelizeMeta',
-  migrations: ['database/migrations'],
+	dialect: process.env.DB_DIALECT || "mysql",
+	host: process.env.DB_HOST,
+	username: process.env.DB_USER,
+	password: process.env.DB_PASS,
+	database: process.env.DB_NAME,
+	port: process.env.DB_PORT || 3306,
+	define: {
+		timestamps: true,
+	},
+	migrationStorage: "sequelize",
+	migrationStorageTableName: "SequelizeMeta",
+	migrations: ["database/migrations"],
 };
