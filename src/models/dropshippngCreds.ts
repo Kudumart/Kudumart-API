78eab095cddff6be1a069755fd9709eb7a27f426
Anuolu-2020 <Anuolu1645@gmail.com>
2025-08-09 18:34:43 +0100

    feat: add DropShippingCred model
    This commit introduces the DropShippingCred model for managing
    dropshipping credentials. It includes fields for access tokens, refresh
    tokens, user information, and other relevant details. The model is
    initialized using Sequelize and defines the schema for the `products`
    table.

---
+++ src/models/dropshippngCreds.ts
@@ -0,0 +1,92 @@
+import { DataTypes, Model, Sequelize } from "sequelize";
+
+class DropShippingCred extends Model {
+	id!: string;
+	accessToken!: string;
+	expiresIn!: number;
+	expireTime!: number;
+	refreshToken!: string;
+	refreshExpiresIn!: number;
+	refreshTokenValidTime!: number;
+	userId!: string;
+	userNick!: string;
+	locale!: string | null;
+	accountId!: string | null;
+	accountPlatform!: string | null;
+	sellerId!: string | null;
+	createdAt!: Date;
+	updatedAt!: Date;
+
+	static associate(models: any) {}
+}
+
+const initModel = (sequelize: Sequelize) => {
+	DropShippingCred.init(
+		{
+			id: {
+				type: DataTypes.UUID,
+				autoIncrement: true,
+				primaryKey: true,
+				allowNull: false,
+			},
+			accessToken: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			expiresIn: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			expireTime: {
+				type: DataTypes.NUMBER,
+				allowNull: false,
+			},
+			refreshToken: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			refreshExpiresIn: {
+				type: DataTypes.INTEGER,
+				allowNull: false,
+			},
+			refreshTokenValidTime: {
+				type: DataTypes.NUMBER,
+				allowNull: false,
+			},
+			userId: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			userNick: {
+				type: DataTypes.STRING,
+				allowNull: false,
+			},
+			locale: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			accountId: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			accountPlatform: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+			sellerId: {
+				type: DataTypes.STRING,
+				allowNull: true,
+			},
+		},
+		{
+			sequelize,
+			modelName: "Product",
+			timestamps: true,
+			paranoid: false,
+			tableName: "products",
+		},
+	);
+};
+
+export default DropShippingCred;
+export { initModel };
