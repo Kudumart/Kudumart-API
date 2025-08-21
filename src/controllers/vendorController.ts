85e206e11628c4a57adc94fd171e5ece4bc91e22
Anuolu-2020 <Anuolu1645@gmail.com>
2025-08-21 17:47:59 +0100

    fix(vendor): Remove unused mail service imports

--- src/controllers/vendorController.ts
+++ src/controllers/vendorController.ts
@@ -3,8 +3,6 @@ import { Request, Response, NextFunction } from "express";
 import User from "../models/user";
 import { v4 as uuidv4 } from "uuid";
 import { Op, ForeignKeyConstraintError, Sequelize } from "sequelize";
-import { sendMail } from "../services/mail.service";
-import { emailTemplates } from "../utils/messages";
 import logger from "../middlewares/logger"; // Adjust the path to your logger.js
 import { AuthenticatedRequest } from "../types/index";
 import KYC from "../models/kyc";
