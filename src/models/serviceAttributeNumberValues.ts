3bb17a4ab6d313368dfc96efdcd5fdc5580480b3
Anuolu-2020 <Anuolu1645@gmail.com>
2025-08-25 19:27:45 +0100

    feat(models): associate AttributeDefinitions model
    This change associates the `AttributeDefinitions` model to
    `ServiceAttributeNumberValues` and updates the foreign key references
    and table names accordingly. It also adds a default value for the id
    field.

--- src/models/serviceAttributeNumberValues.ts
+++ src/models/serviceAttributeNumberValues.ts
@@ -1,18 +1,21 @@
 import { DataTypes, Model, Sequelize } from "sequelize";
+import AttributeDefinitions from "./attributeDefinitions";
 
 export class ServiceAttributeNumberValues extends Model {
 	public id!: string;
 	public service_id!: string;
 	public attribute_id!: number;
 	public value!: number;
+	public attribute!: AttributeDefinitions;
 
 	public static associate(models: any) {
 		this.belongsTo(models.Services, {
 			foreignKey: "service_id",
+			onDelete: "CASCADE",
 			targetKey: "id",
 			as: "service",
 		});
-		this.belongsTo(models.ServiceAttributes, {
+		this.belongsTo(models.AttributeDefinitions, {
 			foreignKey: "attribute_id",
 			targetKey: "id",
 			as: "attribute",
@@ -26,6 +29,7 @@ const initModel = (sequelize: Sequelize) => {
 			id: {
 				type: DataTypes.UUIDV4,
 				primaryKey: true,
+				defaultValue: DataTypes.UUIDV4,
 			},
 			service_id: {
 				type: DataTypes.UUID,
@@ -39,7 +43,7 @@ const initModel = (sequelize: Sequelize) => {
 				type: DataTypes.INTEGER,
 				allowNull: false,
 				references: {
-					model: "service_attribute",
+					model: "attribute_definitions",
 					key: "id",
 				},
 			},
@@ -49,13 +53,13 @@ const initModel = (sequelize: Sequelize) => {
 			},
 		},
 		{
-			tableName: "service_attribute_number_values",
+			tableName: "attribute_number_values",
 			timestamps: false,
 			sequelize,
 			indexes: [
 				{
 					unique: true,
-					name: "unique_service_attribute_number_value",
+					name: "unique_attribute_number_value",
 					fields: ["service_id", "attribute_id"],
 				},
 			],
